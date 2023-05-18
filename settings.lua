767d65556891499aaafa83ea496a154939cee3ec
Jackson LaMotte <kite7742@gmail.com>
2023-05-18 18:21:00 -0500

    Adds comments explaining settings

--- settings.lua
+++ settings.lua
@@ -1,7 +1,10 @@
+-- Which side the computer will look for an inventory.
 local INVENTORY_SIDE = "right"
 
+-- Which side the computer will output a redstone signal to.
 local REDSTONE_OUTPUT_SIDE = "left"
 
+-- Which key should be pressed to exit the program
 local STOP_KEY = keys.q
 
 return {INVENTORY_SIDE = INVENTORY_SIDE, REDSTONE_OUTPUT_SIDE = REDSTONE_OUTPUT_SIDE, STOP_KEY = STOP_KEY}
